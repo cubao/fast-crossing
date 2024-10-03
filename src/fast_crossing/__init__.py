ef7d62b9d08ba63fdf261f3978c26d742960b468
tang zhixiong <zhixiong.tang@momenta.ai>
2024-10-03 16:53:25 +0800

    fix

--- src/fast_crossing/__init__.py
+++ src/fast_crossing/__init__.py
@@ -1,2 +1,4 @@
-from ._core import *  # noqa: PGH004
-from ._core import __version__  # noqa: PGH004
+from __future__ import annotations
+
+from ._core import * # noqa: F403
+from ._core import __version__
